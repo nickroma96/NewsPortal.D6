72498de932be7b0748d423e1cd97e8efe8480211
Nikita Romanenko <noise_bomb94@mail.ru>
2022-03-07 09:27:25 +0400

    Update project from d5

---
+++ NewsPaper/sign/urls.py
@@ -0,0 +1,32 @@
+"""sign URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import BaseRegisterView
+from .views import upgrade_me
+
+urlpatterns = [
+    path('login/',
+         LoginView.as_view(template_name='sign/login.html'),
+         name='login'),
+    path('logout/',
+         LogoutView.as_view(template_name='sign/logout.html'),
+         name='logout'),
+    path('signup/',
+         BaseRegisterView.as_view(template_name='sign/signup.html'),
+         name='signup'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+]
